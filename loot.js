5d61244a9c10bae2a09cf2f3d8525b6f4828c40b
Spencer Lazzar <spencerlazzar@yahoo.com>
2020-11-09 11:31:14 -0800

    Inital outline and files

---
+++ loot.js
@@ -0,0 +1,38 @@
+// loot items maybe like this?
+
+// by class
+class Loot {
+  constructor(name, role, health, attack)
+}
+
+// or as an object of objects
+let loot = {
+  small_bandage = {
+    name: 'small bandage',
+    role: ['wizard', 'assassin', 'warrior', 'hunter'],
+    health: 1,
+    attack: 0
+  },
+
+  large_bandage = {
+    name: 'large bandage',
+    role: ['wizard', 'assassin', 'warrior', 'hunter'],
+    health: 2,
+    attack: 0
+  },
+
+  magical_amulet = {
+    name: 'magical amulet',
+    role: ['wizard'],
+    health: 0,
+    attack: 2
+  },
+
+  enchanted_sword_max = {
+
+  },
+
+  enchanted_sword_min = {
+
+  }
+}
---
+++ scenario.js
@@ -0,0 +1,5 @@
+// put scenarios in here? Maybe a scenario class with name, dialogue, choices, next? IDK yet... We might need to attach loot objects to the scenarios
+
+class Senario {
+  constructor(name, dialogue, choices)
+}
