a09a4053c84ca6c7ef5290a8210d7af02c8dea8c
Spencer Lazzar <spencerlazzar@yahoo.com>
2020-11-09 16:50:48 -0800

    Scenario, Characters, and more

--- loot.js
+++ loot.js
@@ -5,34 +5,34 @@ class Loot {
   constructor(name, role, health, attack)
 }
 
-// or as an object of objects
-let loot = {
-  small_bandage = {
-    name: 'small bandage',
-    role: ['wizard', 'assassin', 'warrior', 'hunter'],
-    health: 1,
-    attack: 0
-  },
+let orcLordMace = new Loot('Orc Lord Mace', ['Ogre'], 0, 2);
+let herbalPotion = new Loot('Herbal Potion', ['Wizard', 'Assassin', 'Warrior', 'Hunter'], 5, 0);
+let strongBandages = new Loot('Strong Bandages', ['Wizard', 'Assassin', 'Warrior', 'Hunter'], 10, 0);
+let falcon = new Loot('Falcon', ['Hunter'], 0, 0);
+let sheild = new Loot('Sheild', ['Ogre'], 3, 2);
+let poisonousBerries = new Loot('Poinsonous Berries', ['Assassin'], 0, 3);
+let magicalAmulet = new Loot('Magical Amulet', ['Wizard'], 3, 2);
+let hoodAndJesses = new Loot('Hood and Jesses', ['Hunter'], 2, 3);
+let strengthSpell = new Loot('Strength Spell', ['Wizard', 'Assassin', 'Warrior', 'Hunter'], 0, 1);
+let gnarledStaff = new Loot('Gnarled Staff', ['Wizard'], 3, 2);
+let strongHeirloomBow = new Loot('Strong Heirloom Bow', ['Hunter'], 0, 6);
+let brittleHeirloomBow = new Loot('Brittle Heirloom Bow', ['Hunter'], 0, 1);
+let mightyEnchantedSword = new Loot('Mighty Enchanted Sword', ['Assassin'], 0, 6);
+let mysteriousSword = new Loot('Mysterious Sword', ['Assassin'], 0, 1);
 
-  large_bandage = {
-    name: 'large bandage',
-    role: ['wizard', 'assassin', 'warrior', 'hunter'],
-    health: 2,
-    attack: 0
-  },
-
-  magical_amulet = {
-    name: 'magical amulet',
-    role: ['wizard'],
-    health: 0,
-    attack: 2
-  },
-
-  enchanted_sword_max = {
-
-  },
-
-  enchanted_sword_min = {
-
-  }
+modules.export = {
+  orcLordMace,
+  herbalPotion,
+  strongBandages,
+  falcon,
+  sheild,
+  poisonousBerries,
+  magicalAmulet,
+  hoodAndJesses,
+  strengthSpell,
+  gnarledStaff,
+  strongHeirloomBow,
+  brittleHeirloomBow,
+  mightyEnchantedSword,
+  mysteriousSword
 }
